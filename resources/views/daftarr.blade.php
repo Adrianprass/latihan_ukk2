2274c810b762e2402420da0694d6ce18077b9396
“Adrianprass” <alifadrian96@gmail.com>
2021-03-30 11:43:47 +0700

    initial commit

---
+++ resources/views/daftarr.blade.php
@@ -0,0 +1,96 @@
+@extends('daftars.layout')
+  
+@section('content')
+<div class="row">
+    <div class="col-lg-12 margin-tb">
+        <div class="pull-left">
+            <h2>Penerimaan Peserta Didik SMK Wikrama Bogor</h2>
+        </div>
+        <div class="pull-right">
+            <a class="btn btn-primary" href="{{ url()->previous() }}"> Back</a>
+        </div>
+    </div>
+</div>
+   
+@if ($errors->any())
+    <div class="alert alert-danger">
+        <strong>Whoops!</strong> Data Gagal Disimpan.<br><br>
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
+   
+<form action="{{ route('daftars.store') }}" method="POST">
+    @csrf
+  
+     <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Nis:</strong>
+                <input type="text" name="nis" class="form-control" placeholder="NIS">
+            </div>
+        </div>
+     <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Nama:</strong>
+                <input type="text" name="nama" class="form-control" placeholder="Nama">
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Jenis Kelamin:</strong>
+                <input type="text" name="jenis_kelamin" class="form-control" placeholder="Jenis kelamin">
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Tempat Lahir:</strong>
+                <input type="text" name="tempat_lahir" class="form-control" placeholder="Tempat lahir">
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Tanggal Lahir:</strong>
+                <input type="date" name="tanggal_lahir" class="form-control" placeholder="Tanggal lahir">
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Alamat:</strong>
+                <input type="text" name="alamat" class="form-control" placeholder="Alamat">
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Asal Sekolah:</strong>
+                <input type="text" name="asal_sekolah" class="form-control" placeholder="Asal sekolah">
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Kelas:</strong>
+                <input type="text" name="kelas" class="form-control" placeholder="Kelas">
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Jurusan</strong>
+                <input type="text" name="jurusan" class="form-control" placeholder="Jurusan">
+            </div>
+        </div>   
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Foto Peserta</strong>
+                <input type="file" name="foto" class="form-control" placeholder="Foto">
+            </div>
+        </div>      
+        <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+                <button type="submit" class="btn btn-primary">Submit</button>
+        </div>
+    </div>
+   
+</form>
+@endsection
